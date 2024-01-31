8e9ea2906fa71de300ae1df170de74d28d2855ac
yung-khajiit <jeenriquez1219@gmail.com>
2024-01-31 08:46:55 -0600

    Add Transactions component working

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
   import Header from './components/Header.vue';
   import Balance from './components/Balance.vue';
   import IncomeExpenses from './components/IncomeExpenses.vue';
+  import AddTransaction from './components/AddTransaction.vue';
   import {ref, computed} from 'vue'
 
-  const transactions = ref([
-    {id: 1, text: 'Paycheck', amount: 699.99},
-    {id: 2, text: 'Food', amount: -35},
-    {id: 3, text: 'Cat Treats', amount: -25},
-    {id: 4, text: 'Bill', amount:-200},
-    {id: 5, text: 'Tigers Vet', amount:-80},
-  ])
+  const transactions = ref([])
+
+  // const transactions = ref([
+  //   {id: 1, text: 'Paycheck', amount: 699.99},
+  //   {id: 2, text: 'Food', amount: -35},
+  //   {id: 3, text: 'Cat Treats', amount: -25},
+  //   {id: 4, text: 'Bill', amount:-200},
+  //   {id: 5, text: 'Tigers Vet', amount:-80},
+  // ])
 
   //get total
   const total = computed(() => {
@@ -27,7 +30,7 @@
       return acc + transaction.amount
     }, 0)
   })
-  
+
 // get the expense by adding all negative values
   const expense = computed( () => {
     return transactions.value
@@ -37,6 +40,14 @@
     }, 0)
   })
 
+//handle transaction submitted
+  const handleTransactionSubmitted = (transactionData) => {
+    transactions.value.push({
+      text: transactionData.text,
+      amount: transactionData.amount,
+    })
+  }
+
 </script>
 
 <template>
@@ -44,6 +55,8 @@
   <div class="container">
     <Balance :total="total"></Balance> 
     <IncomeExpenses :income="income" :expense="expense"></IncomeExpenses>
+    <AddTransaction @transactionSubmitted="handleTransactionSubmitted"></AddTransaction>
+    <!-- {{ transactions }} -->
   </div>
 
 
